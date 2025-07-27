ee90c7224cc79a729ca4cc7301b583882d325468
clnt <c.connery14@gmail.com>
2025-07-27 11:02:57 +0100

    complete next dashboard

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { fetchCustomers } from "@/app/lib/actions";
+import { lusitana } from "@/app/ui/fonts";
 
-const CustomerPage = () => {
-  return <div>CustomerPage</div>;
+const CustomerPage = async () => {
+  const customers = await fetchCustomers();
+  console.log(customers);
+
+  const CustomerCard = ({
+    name,
+    email,
+    id,
+  }: {
+    name: string;
+    email: string;
+    id: string;
+  }) => (
+    <>
+      <div className="mb-2 p-2 border flex flex-col items-center rounded-lg">
+        <h1 className="">{name}</h1>
+        <p className="font-extralight">{email}</p>
+      </div>
+    </>
+  );
+
+  return (
+    <div>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      {customers?.map((customer) => (
+        <CustomerCard
+          key={customer.id}
+          name={customer.name}
+          email={customer.email}
+          id={customer.id}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default CustomerPage;
